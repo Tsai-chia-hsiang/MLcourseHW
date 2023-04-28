7acc16f1af714b8aa248ef23da83019ff9e07bbd
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2023-04-29 05:52:56 +0800

    re-writing

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,14 +1,14 @@
 import numpy as np
 import os
 from tqdm import tqdm
-from Dataset import LIBSVM_Dataset
-from libsvm.svmutil import *
+from LibSVMextension import cross_validation, LIBSVM_Dataset
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+
 
 def makedir(d):
     if not os.path.exists(d):
         os.mkdir(d)
     return d
-
 
 def generate_data(region=(-100, 100), N=500, gaussian_noise_para=(0.0,1.0)):
     
@@ -16,7 +16,7 @@ def generate_data(region=(-100, 100), N=500, gaussian_noise_para=(0.0,1.0)):
     y = 2x+eps~Normal(guassian_noise_para)
     """
     print("data information:")
-    print(f"N: {N}, xregion:{region[0]}~{region[1]}")
+    print(f"N: {N}, xregion:{region[0]}~{region[1]}", end= " ")
     print(f"N({gaussian_noise_para[0]}, {gaussian_noise_para[1]})")
     
     x = np.linspace(region[0], region[1], N, dtype=np.float64).reshape(-1,1)
@@ -31,34 +31,44 @@ def generate_data(region=(-100, 100), N=500, gaussian_noise_para=(0.0,1.0)):
 
 def main(saveroot):
     
-    datasaveroot = makedir(os.path.join(saveroot, "data"))
-    
-    dataset = LIBSVM_Dataset()
-    data = dataset.get_data(fromfile=datasaveroot)
-    
-    """
+
     dataset = LIBSVM_Dataset(generator=generate_data)
-    data = dataset.gen(
-        k_fold=5, savepath=datasaveroot,
-        region=(-100, 100), N=500, gaussian_noise_para=(0.0,1.0)
-    )
-    """
+    dataset.gen(needreturn=False)
     
-    for k,v in data.items():
-        print(k)
-        print("="*10)
-        if isinstance(v, list):
-            for vi in v:
-                for ki, vii in vi.items():
-                    print(f"    {ki}")
-                print("   ","="*10)
-            
+    _ = LIBSVM_Dataset.savedata(
+        data=dataset.get_data(),
+        savepath=makedir(os.path.join("result","data"))
+    )
+
+    pc = np.arange(-5, 17, 2)
+    pg = np.arange(-15, 5, 2)
+    parameters={
+        'c':np.power(np.ones(pc.shape[0])*2 ,pc),
+        'g':np.power(np.ones(pg.shape[0])*2 ,pg)
+    }
+
+    cross_validation(
+        parameters=parameters,
+        data = dataset, 
+        saveroot= makedir(os.path.join(saveroot,"origin"))
+    )
+
+    cross_validation(
+        parameters=parameters,
+        data =dataset.scaling(scaler=MinMaxScaler((0,1)), label=False), 
+        saveroot=makedir(os.path.join(saveroot,"scaling_01"))
+    )
+    
+    cross_validation(
+        parameters=parameters,
+        data = dataset.scaling(scaler=StandardScaler(), label=False),
+        saveroot=makedir(os.path.join(saveroot,"std"))
+    )
+    
         
-
-
 if __name__ == "__main__":
+    
     saveroot = os.path.join("result")
     if not os.path.exists(saveroot):
         os.mkdir(saveroot)
-        
     main(saveroot=saveroot)
