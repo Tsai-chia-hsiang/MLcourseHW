8fface743e8091adf86ca60b96d53b1fd75aeba4
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2023-06-25 21:12:30 +0800

    c

--- Backpropagation/cmp_opt.py
+++ Backpropagation/cmp_opt.py
@@ -5,12 +5,18 @@ import os
 from NN.Layers.Activation import tanh_activation
 from NN.Layers.Linear import Linear
 from NN.Layers.Transform import BatchNormalization
+from NN.Loss.loss import _loss
 from NN.Loss.MSE import MSEloss
 from NN.Model import NNmodel, test
 from NN.Optimizer.optr import _optimizer
 from NN.Optimizer.SGD import SGDopt
 from NN.Optimizer.Ada import AdaGrad
 from vis.plotchange import plot_changes
+
+def makepath(p:os.PathLike)->os.PathLike:
+    if not os.path.exists(p):
+        os.mkdir(p)
+    return p
 
 class testNN(NNmodel):
     
@@ -42,92 +48,105 @@ def loaddata(root:os.PathLike):
     return xtrain, ytrain, xval, yval, xtest, ytest 
 
 def train(
-    xtrain:np.ndarray, ytrain:np.ndarray, 
-    xval:np.ndarray, yval:np.ndarray, 
-    modelframe:type[NNmodel], modelarch:list, opt:type[_optimizer], 
-    hyp:dict, modelsavepath:os.PathLike,
-    epochs=100, bsize = 32,loadpretrain:os.PathLike=None
+    xtrain:np.ndarray, ytrain:np.ndarray, xval:np.ndarray, yval:np.ndarray, 
+    Model:NNmodel, LossFunction:type[_loss], opt:type[_optimizer], 
+    hyp:dict, modelsavepath:os.PathLike, epochs=100, bsize = 32
 )->tuple:
     
-    model = modelframe(
-        in_dim=xtrain.shape[1], out_dim=ytrain.shape[1], 
-        transition=modelarch
-    )
-    if loadpretrain is not None:
-        model.loadmodel(loadpretrain)
-    
-    Loss = MSEloss()
-
-    trainLoss = [] ; valLoss = []
+    model = copy.deepcopy(Model)
+    Loss = LossFunction()
     optimizer = opt(
         model=model, lr=hyp['lr'],
         **{k:v for k,v in hyp.items() if k!= 'lr'}
     )
+    trainLoss, valLoss= [] ,[]
     
     pbar = tqdm(range(epochs))
-    minmse = np.inf
-    for e in pbar:
+    minl = np.inf
+    for _ in pbar:
         
         #A batch
-        trainmse = 0
+        trainl = 0
         for b in range(0,xtrain.shape[0], bsize):
             
             yhat = model.forward(x=xtrain[b:b+bsize, :])
-            trainmse += Loss(yhat=yhat, y=ytrain[b:b+bsize, :])
+            trainl += Loss(yhat=yhat, y=ytrain[b:b+bsize, :])/(xtrain.shape[0]/bsize)
 
             optimizer.backward(loss=Loss, batch_notation=True)
             optimizer.update()
-
-        trainLoss.append(trainmse/(xtrain.shape[0]/bsize))
         
-        valmse = Loss(
+        trainLoss.append(trainl)
+        
+        vall = Loss(
             yhat=model.forward(x=xval, req_g=False), 
             y=yval, req_g=False
         )
-        valLoss.append(valmse)
-        if valmse < minmse:
+        valLoss.append(vall)
+        pbar.set_postfix_str(
+            f"train:{trainl:.5f}, val:{vall:.5f}, bestva:{minl:.5f}"
+        )
+        if vall < minl:
             model.savemodel(saveat=modelsavepath)
-            minmse = valmse
+            minl = vall
 
-    bestmodel = modelframe(
+    model.loadmodel(f"{modelsavepath}.npz")
+    return model, trainLoss, valLoss
+
+
+
+def Comparsion_different_optimizer(
+    sampledatadir:os.PathLike, cmp:dict, pretrain_path :os.PathLike,
+    modelsaveroot:os.PathLike, resultsaveroot:os.PathLike
+):
+
+    xtrain, ytrain, xval, yval, xtest, ytest  = loaddata(sampledatadir)
+
+    model = testNN(
         in_dim=xtrain.shape[1], out_dim=ytrain.shape[1], 
-        transition=modelarch
+        transition=[16, 16, 8, 8, 4, 4]
     )
-    bestmodel.loadmodel(f"{modelsavepath}.npz")
-    return bestmodel, trainLoss, valLoss
-
-
-
-if __name__ == "__main__":
-
-    xtrain, ytrain, xval, yval, xtest, ytest  = loaddata(os.path.join("gendata"))
-    opt_type = [SGDopt, SGDopt, AdaGrad]
-    hyp = [{'lr':0.001, 'weight_decay':0.1},
-           {'lr':0.001, 'momentum':0.9, 'weight_decay':0.1},
-           {'lr':0.15, 'weight_decay':0.1}
-        ]
-    model_name = ["sgd","sgd_mom09", "ada"]
+    model.loadmodel(pretrain_path)
+    
     trainlosses = []
     vallossses = []
-    for opti, hypi, name in zip(opt_type, hyp, model_name):
+    for opti, hypi, name in zip(cmp['optr'], cmp['hyp'], cmp['model_name']):
         print(name)
         bmodel, trainloss, valloss = train(
-            xtrain, ytrain, xval, yval,
-            modelframe=testNN, modelarch=[16, 16, 8, 8, 4, 4],
-            opt=opti, hyp=hypi,
-            modelsavepath=os.path.join("model", name),
-            loadpretrain=os.path.join("model","random_para.npz" )
+            xtrain, ytrain, xval, yval,Model=model, 
+            opt=opti, hyp=hypi, LossFunction=MSEloss,
+            modelsavepath=os.path.join(modelsaveroot, name),
         )
         testLoss = test(xtest, ytest, bmodel, MSEloss)
-        print(testLoss)
+        print(f"testing loss : {testLoss:.5f}")
         trainlosses.append(trainloss)
         vallossses.append(valloss)
 
     plot_changes(
-        l=trainlosses, name=model_name, title="Train loss",
-        saveto="trainloss.jpg"
+        l=trainlosses, name=cmp['model_name'], title="Train loss",
+        saveto=os.path.join(resultsaveroot,"trainloss.jpg")
     )
     plot_changes(
-        l=vallossses, name=model_name, title="val loss",
-        saveto="valloss.jpg"
+        l=vallossses, name=cmp['model_name'], title="val loss",
+        saveto=os.path.join(resultsaveroot,"valloss.jpg")
     )
+
+
+if __name__ == "__main__":
+
+    cmp_optr = {
+        'optr':[SGDopt, SGDopt, AdaGrad],
+        'hyp':[
+            {'lr':0.001, 'weight_decay':0.1},
+            {'lr':0.001, 'momentum':0.9, 'weight_decay':0.1},
+            {'lr':0.15, 'weight_decay':0.1}
+        ],
+        'model_name':["sgd","sgd_mom09", "ada"]
+    }
+
+    Comparsion_different_optimizer(
+        sampledatadir=os.path.join('gendata'),
+        cmp=cmp_optr, pretrain_path=os.path.join("model", "random_para.npz"),
+        modelsaveroot=os.path.join("model"),
+        resultsaveroot=makepath(os.path.join("comparsion_optrs"))
+    )
+
